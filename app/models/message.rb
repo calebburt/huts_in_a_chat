3e31e4eece97ef68baa3676c3ffa9187b9249411
Caleb Burt <calebburt@outlook.com>
2026-03-18 17:43:26 +1100

    actually actually fix it

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
-  belongs_to :chat, dependent: :destroy
+  belongs_to :chat
   belongs_to :user
 
   validates :content, presence: true, if: -> { !attachment.attached? }
